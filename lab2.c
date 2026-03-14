eb4c6a5a068f94063fc5b7b4b6cb53c0592efab4
BestFansi <baigeisnfansi@outlook.com>
2026-03-14 11:38:11 +1000

    Create lab2.c

---
+++ lab2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	double matrix[3][3]; 
+	double a,b;
+	int i,j; 
+	/*for (i = 0; i < 3; i++) { 
+    	for (j = 0; j < 3; j++) {
+    		scanf("%lf", &matrix[i][j]);
+    	}
+    }
+    for (i = 0; i < 3; i++) {
+		sub_main += matrix[i][i];
+    	sub_sec += matrix[i][2-i];
+	}	
+	printf("main diagonary= %lf\n", a);
+	printf("secondary diagonary = %lf\n", b);
+	*/
+	double A[2][2];
+	double B[2][2];
+	for (i = 0; i < 2; i++) { 
+    	for (j = 0; j < 2; j++) {
+    		scanf("%lf", &A[i][j]);
+    	}
+	}
+	
+	for (i = 0; i < 2; i++) {
+		
+	}
+	printf("result = %lf", &B);
+	return 0;
+}
