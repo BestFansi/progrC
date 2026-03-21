8c8d28a6fe7aae042c4a9d87ede9d7b76da8962a
BestFansi <baigeisnfansi@outlook.com>
2026-03-21 11:36:32 +1000

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,47 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
-	double matrix[3][3]; 
+	//1
+	int M=3;
+	double matrix[M][M]; 
 	double m,n;
-	int i,j; 
-	for (i = 0; i < 3; i++) { 
+	int i,j,k; 
+	/*for (i = 0; i < 3; i++) { 
     	for (j = 0; j < 3; j++) {
     		scanf("%lf", &matrix[i][j]);
     	}
     }
     for (i = 0; i < 3; i++) {
-		sub_main += matrix[i][i];
-    	sub_sec += matrix[i][2-i];
+		m += matrix[i][i];
+    	n += matrix[i][2-i];
 	}	
-	printf("main diagonary= %lf\n", a);
-	printf("secondary diagonary = %lf\n", b);
+	printf("main diagonary= %lf\n", m);
+	printf("secondary diagonary = %lf\n", n);
 	
-	
-	double A[2][2];
-	double B[2][2];
-	double a,b,c,d;
+	//2
+	int N=2;
+	int A[N][N];
+	int B[N][N];
 	for (i = 0; i < 2; i++) { 
     	for (j = 0; j < 2; j++) {
-    		scanf("%lf", &A[i][j]);
+    		B[i][j]=0;
     	}
 	}
-	
-	a = A[0][0];
-    b = A[0][1];
-    c = A[1][0];
-    d = A[1][1];
-    
-    B[0][0] = a * a + b * c;
-    B[0][1] = a * b + b * d;
-    B[1][0] = a * c + c * d;
-    B[1][1] = b * c + d * d;
+	for (i = 0; i < 2; i++) { 
+    	for (j = 0; j < 2; j++) {
+    		scanf("%d", &A[i][j]);
+    	}
+	}
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            for (k = 0; k < 2; k++) {
+            	B[i][j] += A[i][k] * A[k][j];
+			}
+        }
+    }
     
 	for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++) {
-            printf("%.2lf\t", B[i][j]);
+            printf("%d\t", B[i][j]);
         }
         printf("\n");
     }
