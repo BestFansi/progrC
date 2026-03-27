1c77a909a43467df8c027322ef072275ef4c9def
BestFansi <baigeisnfansi@outlook.com>
2026-03-28 09:27:18 +1000

    Delete New folder directory

--- New folder/tri.c
+++
@@ -1,9 +0,0 @@
-#include "tri.h"
-#include <math.h>
-double perimeter(double a, double b, double c) {
-	return a + b + c;
-}
-double area(double a, double b, double c) {
-	double p = perimeter(a, b, c) / 2;
-	return sqrt(p * (p - a) * (p - b) * (p - c));
-}
