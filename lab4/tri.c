ca2d3642d582413c17e9709128db3ae9627c8dd8
BestFansi <baigeisnfansi@outlook.com>
2026-03-28 09:27:53 +1000

    Add files via upload

---
+++ lab4/tri.c
@@ -0,0 +1,9 @@
+#include "tri.h"
+#include <math.h>
+double perimeter(double a, double b, double c) {
+	return a + b + c;
+}
+double area(double a, double b, double c) {
+	double p = perimeter(a, b, c) / 2;
+	return sqrt(p * (p - a) * (p - b) * (p - c));
+}
