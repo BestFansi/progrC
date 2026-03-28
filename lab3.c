61fb3bb3ab113b892e9519ebb427ac3b48840367
BestFansi <baigeisnfansi@outlook.com>
2026-03-28 11:14:14 +1000

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -6,9 +6,23 @@ int main() {
 	char str[81];
 	scanf("%s", str);
 	int len = strlen(str);
+	/*
 	for (int i = 0; i < len; ++i) {
 		str[i] = toupper(str[i]);
 	}
+	*/
+	for (int i = 0; i < len; i++) {
+		if (str[i] == 'a') {
+			str[i] = 'A';
+		}
+		else if (str[i] == 'b') {
+			str[i] = 'B';
+		}
+		else if (str[i] == '!') {
+			str[i] = '?';
+		}
+	}
+
 	printf("%s", str);
 	return 0;
 }
