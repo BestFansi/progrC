4554fbb42966836c58103269b03a5d532bba466e
BestFansi <baigeisnfansi@outlook.com>
2026-04-03 11:29:55 +1000

    delete

--- lab5/matrix.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATRIX_H
-#define MATRIX_H
-double plus(int n, double A[n][n], double B[n][n]);
-double minus(int n, double A[n][n], double B[n][n]);
-double time(int n, double A[n][n], double B[n][n]);
-#endif
