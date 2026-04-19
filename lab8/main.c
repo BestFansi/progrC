dab7d48ce73ca0dc14c8289159c62cbf1aa7327b
BestFansi <baigeisnfansi@outlook.com>
2026-04-19 23:56:44 +1000

    1

--- lab8/main.c
+++ lab8/main.c
@@ -6,3 +6,59 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <local.h>
+typedef struct {
+  char name[20];
+  int birthyear;
+  char gender[3];
+  double height;
+} humen;
+
+int select() {
+  char a[10];
+  printf("по какому элементу упорядочить? (имя, год, пол, рост)");
+  scanf("%s", a);
+  while (1) {
+    if (a == 'имя') {
+      return 1;
+      break;
+    }
+    else if (a == 'год') {
+      return 2;
+      break;
+    }
+    else if (a == 'пол') {
+      return 3;
+      break;
+    }
+    else if (a == 'рост') {
+      return 4;
+      break;
+    }
+    else {
+      printf("Ошибка! Введите заново");
+    }
+  }
+}
+void bubble_sort(humen[]) {
+
+}
+int main() {
+  humen arr[4];
+  //create file
+  FILE* file = fopen("records.txt", "w");
+  if (file == NULL) {
+    return 1;
+  }
+  fprintf(file, "Антон 1997 муж 1,78");
+  fprintf(file, "Павел 2006 муж 1,82");
+  fprintf(file, "Лю 2004 муж 1,88");
+  fprintf(file, "Лена 1983 жен 1,68");
+  for (int i = 0; i < 4; i++) {
+    fscanf(file, "%s %d %s %f", arr[i].name, arr[i].birthyear, arr[i].gender, arr[i].height);
+  }
+  fclose(file);
+
+  
+  return 0;
+}
