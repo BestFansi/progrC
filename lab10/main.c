1abd8e3ca07b0a9a6895fc4486e96dee0edfecbc
BestFansi <baigeisnfansi@outlook.com>
2026-05-30 10:40:05 +1000

    add function calculate avg

--- lab10/main.c
+++ lab10/main.c
@@ -1,4 +1,4 @@
-//Написать программу вычисления максимального числа из введенных пользова-телем в виде одной строки.
+//Написать программу вычисления максимального числа из введенных пользователем в виде одной строки.
 //Использовать для этого макрос (без функции), на вход которому подается массив чисел и их количество.
 
 #include <stdio.h>
@@ -13,11 +13,19 @@
         }                           \
     }                               \
 
+#define FIND_AVG(arr, n, avg)       \
+    for (int i = 0; i < n; i++) {   \
+        total += arr[i];            \
+    }                               \
+    avg = (double) total / n;       \
+
 int main() {
     char input[100];
     int arr[100];
-    int i = 0;
+    int count = 0;
     int max;
+    int total = 0;
+    double avg;
 
     printf("Please enter numbers: ");
     fgets(input, 100, stdin);
@@ -25,15 +33,23 @@ int main() {
     char* token = strtok(input, " \n");
 
     while (token != NULL) {
-        arr[i] = atoi(token);
-        i++;
+        arr[count] = atoi(token);
+        count++;
 
         token = strtok(NULL, " \n");
     }
 
-    FIND_MAX(arr, i, max);
+    FIND_MAX(arr, count, max);
+    FIND_AVG(arr, count, avg);
+
+    printf("array:");
+
+    for (int j = 0; j < count; j++) {
+        printf(" %d", arr[j]);
+    }
+    printf("\n");
 
     printf("The biggest number is %d\n", max);
-
+    printf("average = %.2lf\n", avg);
     return 0;
 }
